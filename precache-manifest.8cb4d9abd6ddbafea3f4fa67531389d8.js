14c05523d3949c431437d8a2ec90bc1c91d1a8b4
Rikikudo <trunghieu99tt@gmail.com>
2020-10-12 22:24:10 +0700

    Updates

---
+++ precache-manifest.8cb4d9abd6ddbafea3f4fa67531389d8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87451b7e0e59a2d9b6d321cfe165960a",
+    "url": "/coffins-shop/index.html"
+  },
+  {
+    "revision": "625956689cc61a4c8f57",
+    "url": "/coffins-shop/static/css/2.fe894822.chunk.css"
+  },
+  {
+    "revision": "52507f19da755fc663fb",
+    "url": "/coffins-shop/static/css/main.9d239178.chunk.css"
+  },
+  {
+    "revision": "625956689cc61a4c8f57",
+    "url": "/coffins-shop/static/js/2.e2369ed4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/coffins-shop/static/js/2.e2369ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52507f19da755fc663fb",
+    "url": "/coffins-shop/static/js/main.559a1c96.chunk.js"
+  },
+  {
+    "revision": "3c82b746cdc822315eae",
+    "url": "/coffins-shop/static/js/runtime-main.3bcd5ed1.js"
+  },
+  {
+    "revision": "50e69ccd4a6335eeaeeea7cc68b4b928",
+    "url": "/coffins-shop/static/media/b1.50e69ccd.jpg"
+  },
+  {
+    "revision": "35341f2366891484f6444d608529e1da",
+    "url": "/coffins-shop/static/media/b2.35341f23.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/coffins-shop/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/coffins-shop/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/coffins-shop/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/coffins-shop/static/media/slick.f97e3bbf.svg"
+  }
+]);
